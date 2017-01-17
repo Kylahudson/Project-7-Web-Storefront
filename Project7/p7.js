981563912aea2f740cfa66ff95c8f3cb6d9f298a
Kylahudson <Kyla.hudson432@west-mec.org>
2017-01-17 13:16:43 -0700

    updated

--- Project7/p7.js
+++ Project7/p7.js
@@ -1,26 +1,12 @@
 /**
  * Created by kyla.hudson432 on 1/10/17.
  */
-var background=document.getElementById("leftside");
+function login(){
+document.getElementById("submit").onclick=function(){
+        alert("Successful login!");
+        document.getElementById("b").style.backgroundImage.src="images/shopping.jpg";
+       // window.history.back();
 
-background.onclick=function loop(){
-var shoes=["1","2","3","4","5","6"];
-shoes[0].src="shoes.jpg";
-shoes[1].src="nike.jpg";
-shoes[2].src="adidas.jpg";
-shoes[3].src="vans.jpg";
-shoes[4].src="blackshoes.jpg";
-shoes[5].src="whitevans.jpg";
-
-   var i;
-    for(i=0; i<shoes.length; i++){
-        if (shoes.length) {
-            setTimeout(loop, 70000);
-        }
-    }
+    }}
 
 
-
-
-
-};
